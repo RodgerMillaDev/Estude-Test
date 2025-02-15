f144cafdf8e0360c75da192ce4ec57fbbc385d0a
Rodger Milla Dev <rodgermilladev@gmail.com>
2025-02-16 00:58:40 +0300

    Auth done

--- public/JS/auth.js
+++ public/JS/auth.js
@@ -22,7 +22,7 @@ function toSignUp(){
                 .then((userCred) => {
                     var user = userCred.user;
                     console.log(user);
-                    // window.location.href='index.html'
+                    window.location.href='index.html'
                 })
                 .catch((error) => {
                     document.getElementById("authBtnLog").style.display = "block";
@@ -54,11 +54,38 @@ function toSignUp(){
 
         if(fn && em && pass && cpass){
             if(pass===cpass){
-                firebase.auth().createUserWithEmailAndPassword(em,pass).then((user)=>{
-                   var userId=user.uid
-                   console.log(uid)
-
-                }).catch((error)=>{
+                document.getElementById("authBtnSign").style.display = "none";
+                document.getElementById("authBtnLoderSign").style.display = "flex";
+                firebase.auth().createUserWithEmailAndPassword(em,pass).then((userCred)=>{
+                    var user = (userCred.user)
+                        firebase.firestore().collection("Users").doc(user.uid).set({
+                            name:fn,
+                            em:em,
+                            uid:user.uid
+                        }).then(()=>{
+                        document.getElementById("authBtnSign").style.display="block"
+                        document.getElementById("authBtnLoderSign").style.display="none"
+                        const Toast = Swal.mixin({
+                            toast: true,
+                            position: "top-end",
+                            showConfirmButton: false,
+                            timer: 4000,
+                            timerProgressBar: true,
+                            didOpen: (toast) => {
+                                toast.onmouseenter = Swal.stopTimer;
+                                toast.onmouseleave = Swal.resumeTimer;
+                            }
+                        });
+                        Toast.fire({
+                            icon: "success",
+                            title: "Signed Up successfully."
+                        }).then(()=>{
+                            window.location.href="index.html"
+                        })
+                        })
+                 }).catch((error)=>{
+                    document.getElementById("authBtnSign").style.display = "block";
+                    document.getElementById("authBtnLoderSign").style.display = "none";
                     console.log(error)
                     if(error.code=="auth/email-already-in-use"){
                         Swal.fire({
