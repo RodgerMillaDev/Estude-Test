9ebde157e66195526564352dae5638aa3b96e7fd
Rodger Milla Dev <rodgermilladev@gmail.com>
2025-05-21 16:26:52 +0300

    New Update

--- public/JS/affiliate.js
+++ public/JS/affiliate.js
@@ -1,5 +1,6 @@
 var uid="";
 var em="";
+let userName;
 
 auth.onAuthStateChanged((user)=>{
     if(user){
@@ -7,6 +8,8 @@ auth.onAuthStateChanged((user)=>{
         dbFirestore.collection("Users").doc(uid).get().then((doc)=>{
                  var em=doc.data().em;
                  var uid=doc.data().uid;
+                 userName = doc.data().name;
+;
 
         })
         
@@ -16,22 +19,49 @@ auth.onAuthStateChanged((user)=>{
 })
 function fromAff() {
   if (uid !="") {
-    const affLink = document.getElementById("affLink").value.trim();
-    const expectedDomain = "https://estudetest.web.app/checkout.html";
-    const parts = affLink.split("?");
-    if (
-      parts.length === 3 &&
-      parts[0] === expectedDomain &&
-      parts[1].trim() !== "" && // Title
-      parts[2].trim() !== ""    // Random string
-    ) {
-      const title = parts[1].trim();
-      const newUrl = `${expectedDomain}?${parts[1]}?${parts[2]}`;
-      window.location.href = newUrl;
-    } else {
-      console.error("Invalid link format");
-      Swal.fire("Invalid Link", "Please enter a valid test link.", "error");
-    }
+
+
+     Swal.fire({
+            title: `Are you ${userName}?`,
+            text: `Do you want to continue as ${userName}?`,
+            icon: "question",
+            showCancelButton: true,
+            confirmButtonColor: "#11499E",
+            cancelButtonColor: "#11499E", 
+            confirmButtonText: "No, Sign out",
+            cancelButtonText: "Yes, continue",
+          }).then((result) => {
+            if (result.isConfirmed) {
+                  const affLink = document.getElementById("affLink").value.trim();
+                  const expectedDomain = "https://estudetest.web.app/checkout.html";
+                  const parts = affLink.split("?");
+                  if (
+                    parts.length === 3 &&
+                    parts[0] === expectedDomain &&
+                    parts[1].trim() !== "" && // Title
+                    parts[2].trim() !== ""    // Random string
+                  ) {
+                    const newUrl = `${expectedDomain}?${parts[1]}?${parts[2]}`;
+                    window.location.href = newUrl;
+                  } else {
+                    console.error("Invalid link format");
+                    Swal.fire("Invalid Link", "Please enter a valid test link.", "error");
+                  }
+                // Right User
+            } else if (result.dismiss === Swal.DismissReason.cancel) {
+              auth.signOut().then(() => {
+              window.location.href="index.html";    
+                }).catch((error) => {
+          
+                  console.log(error)
+                  // An error happened.
+                }); 
+                // Wrong user 
+            }
+          });   
+
+
+
   } else {
     Swal.fire("Sign In Required", "Kindly sign in to proceed to test", "info").then(()=>{
       window.location.href="auth.html"
